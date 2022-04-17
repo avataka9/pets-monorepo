9ab799098f7537dd2e2d84b238765c74710332fe
Alex Maruga <avataka9@gmail.com>
2022-04-17 23:21:34 +0300

    .

--- boilerplate/src/App.tsx
+++ boilerplate/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { HomePage, CounterPage, TodosPage } from 'Common/Pages';
 import { Navigation } from 'Common/Components/Navigation';
@@ -9,12 +9,14 @@ import s from './App.module.scss';
 export const App: React.FC = () => {
   return (
     <div className={s.app}>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/counter" element={<CounterPage />} />
-        <Route path="/todos" element={<TodosPage />} />
-      </Routes>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/counter" element={<CounterPage />} />
+          <Route path="/todos" element={<TodosPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
